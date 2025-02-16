ea1425a514e5c9098925e7d21a2d4f4cc4edd5ee
StellRC <38394781+stellRC@users.noreply.github.com>
2025-02-16 03:19:28 +0000

    formatting

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const solutionsMore = document.querySelector("#solutionsMore");
 
 let fullFirst;
 const TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 600;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 600;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
 };
 
 // FORM VARIABLES
@@ -22,264 +22,255 @@ const elPhone = document.getElementsByClassName("phone")[0];
 const elName = document.getElementsByClassName("name")[0];
 const elMessage = document.getElementsByClassName("message")[0];
 const elSubmit = document.getElementsByClassName("input-submit")[0];
-console.log(elSubmit)
+console.log(elSubmit);
 
 const formErrors = {
-    email: false,
-    phone: false,
-    message: false,
-    name: false
+  email: false,
+  phone: false,
+  message: false,
+  name: false,
 };
 
 let hasSubmitted = false;
 
-
 // TYPEWRITER ANIMATION
 
-
 TxtType.prototype.tick = function () {
-    let i = this.loopNum % this.toRotate.length;
-    let fullTxt = this.toRotate[i];
+  let i = this.loopNum % this.toRotate.length;
+  let fullTxt = this.toRotate[i];
 
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  if (fullTxt == this.toRotate[0]) {
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  } else {
+    this.el.innerHTML =
+      '<span class="wrap">' + fullFirst + this.txt + "</span>";
+  }
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
+  let that = this;
+  let delta = (200 - Math.random() * 100) / 3;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    if (fullTxt == this.toRotate[this.toRotate.length - 1]) return;
+    else if (fullTxt == this.toRotate[0]) {
+      fullFirst = this.txt;
+      this.loopNum++;
     } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      delta = this.period;
+      this.isDeleting = true;
     }
-    if (fullTxt == this.toRotate[0]) {
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-    } else {
-        this.el.innerHTML = '<span class="wrap">' + fullFirst + this.txt + '</span>';
-    }
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+  }
 
-    let that = this;
-    let delta = (200 - Math.random() * 100) / 3;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        if (fullTxt == this.toRotate[this.toRotate.length - 1]) return
-        else if (fullTxt == this.toRotate[0]) {
-            fullFirst = this.txt;
-            this.loopNum++;
-        } else {
-            delta = this.period;
-            this.isDeleting = true;
-        }
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-    }
-
-    setTimeout(function () {
-        that.tick();
-    }, delta);
+  setTimeout(function () {
+    that.tick();
+  }, delta);
 };
 
 function showSolutions() {
-    solutionsMore.classList.add("solutions-visible");
-    solutionsMore.classList.remove("solutions-hidden");
-    this.parentElement.classList.add("solutions-hidden")
+  solutionsMore.classList.add("solutions-visible");
+  solutionsMore.classList.remove("solutions-hidden");
+  this.parentElement.classList.add("solutions-hidden");
 }
 
-document.getElementById("solutionsShow").addEventListener("click", showSolutions);
-
+document
+  .getElementById("solutionsShow")
+  .addEventListener("click", showSolutions);
 
 window.onload = function () {
-
-    let elements = document.getElementsByClassName('typewrite');
-    for (let i = 0; i < elements.length; i++) {
-        let toRotate = elements[i].getAttribute('data-type');
-        let period = elements[i].getAttribute('data-period');
-        if (toRotate.toString()) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+  let elements = document.getElementsByClassName("typewrite");
+  for (let i = 0; i < elements.length; i++) {
+    let toRotate = elements[i].getAttribute("data-type");
+    let period = elements[i].getAttribute("data-period");
+    if (toRotate.toString()) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    let carrot = document.createElement("style");
-    HTMLStyleElement.type = "text/css";
-    document.body.appendChild(carrot);
-    // const hiddenSpan = document.querySelector('.span-hidden')
-    // hiddenSpan.style.display = 'none'
-
+  }
+  // INJECT CSS
+  let carrot = document.createElement("style");
+  HTMLStyleElement.type = "text/css";
+  document.body.appendChild(carrot);
+  // const hiddenSpan = document.querySelector('.span-hidden')
+  // hiddenSpan.style.display = 'none'
 };
-
-
-
 
 // FORM
 
-
 validateField({
-    elField: elEmail,
-    validateFn: validateFieldEmail
+  elField: elEmail,
+  validateFn: validateFieldEmail,
 });
 
 validateField({
-    elField: elPhone,
-    validateFn: validateFieldPhone
+  elField: elPhone,
+  validateFn: validateFieldPhone,
 });
 
 validateField({
-    elField: elName,
-    validateFn: validateFieldName
+  elField: elName,
+  validateFn: validateFieldName,
 });
 
 validateField({
-    elField: elMessage,
-    validateFn: validateFieldMessage
+  elField: elMessage,
+  validateFn: validateFieldMessage,
 });
 
 function validateField({ elField, validateFn }) {
-    let touched = false;
+  let touched = false;
 
-    elField.addEventListener("change", (e) => {
-        touched = true; // mark it as touched so that on blur it shows the error.
-    });
+  elField.addEventListener("change", (e) => {
+    touched = true; // mark it as touched so that on blur it shows the error.
+  });
 
-    elField.addEventListener("keyup", (e) => {
-        // remove any error on keyup if existent
-        validateFn(e.target, { removeOnly: true });
+  elField.addEventListener("keyup", (e) => {
+    // remove any error on keyup if existent
+    validateFn(e.target, { removeOnly: true });
 
-        if (hasSubmitted) {
-            updateSubmitSummary();
-        }
-    });
+    if (hasSubmitted) {
+      updateSubmitSummary();
+    }
+  });
 
-    elField.addEventListener("blur", (e) => {
-        if (!touched) return;
-        // show error if touched
-        validateFn(e.target, { live: true });
-    });
+  elField.addEventListener("blur", (e) => {
+    if (!touched) return;
+    // show error if touched
+    validateFn(e.target, { live: true });
+  });
 }
 
-
 function validateFieldEmail(el, opts) {
-    const isEmpty = el.value === "";
+  const isEmpty = el.value === "";
 
-    if (isEmpty) {
-        formErrors.email = true;
-        updateFieldDOM(el, !isEmpty, "Email required.", opts);
-    } else {
-        const isEmailValid = el.value.match(emailRegex);
-        updateFieldDOM(el, isEmailValid, "Invalid email.", opts);
-        formErrors.email = !isEmailValid;
-    }
+  if (isEmpty) {
+    formErrors.email = true;
+    updateFieldDOM(el, !isEmpty, "Email required.", opts);
+  } else {
+    const isEmailValid = el.value.match(emailRegex);
+    updateFieldDOM(el, isEmailValid, "Invalid email.", opts);
+    formErrors.email = !isEmailValid;
+  }
 }
 
 function validateFieldPhone(el, opts) {
-    const isEmpty = el.value === "";
-    updateFieldDOM(el, !isEmpty, "Phone required.", opts);
+  const isEmpty = el.value === "";
+  updateFieldDOM(el, !isEmpty, "Phone required.", opts);
 
-    if (isEmpty) {
-        formErrors.phone = true;
-    } else {
-        const isPhoneValid = el.value.match(phoneRegex);
-        updateFieldDOM(el, isPhoneValid, "Invalid phone.", opts);
-        formErrors.phone = !isPhoneValid;
-    }
+  if (isEmpty) {
+    formErrors.phone = true;
+  } else {
+    const isPhoneValid = el.value.match(phoneRegex);
+    updateFieldDOM(el, isPhoneValid, "Invalid phone.", opts);
+    formErrors.phone = !isPhoneValid;
+  }
 }
 
 function validateFieldName(el, opts) {
-    const isEmpty = el.value === "";
+  const isEmpty = el.value === "";
 
-
-    if (isEmpty) {
-        formErrors.name = true;
-        updateFieldDOM(el, !isEmpty, "Name required.", opts);
-    } else {
-        const isNameValid = true;
-        updateFieldDOM(el, isNameValid, "Invalid", opts);
-        formErrors.name = !isNameValid;
-    }
+  if (isEmpty) {
+    formErrors.name = true;
+    updateFieldDOM(el, !isEmpty, "Name required.", opts);
+  } else {
+    const isNameValid = true;
+    updateFieldDOM(el, isNameValid, "Invalid", opts);
+    formErrors.name = !isNameValid;
+  }
 }
 
 function validateFieldMessage(el, opts) {
-    const isEmpty = el.value === "";
+  const isEmpty = el.value === "";
 
-
-    if (isEmpty) {
-        formErrors.message = true;
-        updateFieldDOM(el, !isEmpty, "Message required.", opts);
-    } else {
-        const isMessageValid = true;
-        updateFieldDOM(el, isMessageValid, "Invalid", opts);
-        formErrors.message = !isMessageValid;
-    }
+  if (isEmpty) {
+    formErrors.message = true;
+    updateFieldDOM(el, !isEmpty, "Message required.", opts);
+  } else {
+    const isMessageValid = true;
+    updateFieldDOM(el, isMessageValid, "Invalid", opts);
+    formErrors.message = !isMessageValid;
+  }
 }
 
 function updateFieldDOM(el, isValid, errorMessage, opts) {
-    const removeOnly = opts?.removeOnly;
-    const isLive = opts?.live;
-    const elField = el.closest(".field");
-    const elError = elField.querySelector(".field-error");
-    let label = el.previousElementSibling.previousElementSibling;
+  const removeOnly = opts?.removeOnly;
+  const isLive = opts?.live;
+  const elField = el.closest(".field");
+  const elError = elField.querySelector(".field-error");
+  let label = el.previousElementSibling.previousElementSibling;
 
-    if (isValid) {
-        // elField.classList.remove("isInvalid");
-        elError.innerText = ""; // It's valid
-        el.removeAttribute("aria-invalid");
-        elError.classList.remove("warning");
-        // el.classList.remove("field-outline");
-        label.classList.remove("error");
-        elSubmit.classList.remove("field-outline");
-    } else if (!removeOnly) {
-        // elField.classList.add("isInvalid");
-        el.setAttribute("aria-invalid", "true");
-        elError.setAttribute("aria-live", isLive ? "assertive" : "off");
-        elError.classList.add("warning");
-        // el.classList.add("field-outline");
-        label.classList.add("error");
-    }
+  if (isValid) {
+    // elField.classList.remove("isInvalid");
+    elError.innerText = ""; // It's valid
+    el.removeAttribute("aria-invalid");
+    elError.classList.remove("warning");
+    // el.classList.remove("field-outline");
+    label.classList.remove("error");
+    elSubmit.classList.remove("field-outline");
+  } else if (!removeOnly) {
+    // elField.classList.add("isInvalid");
+    el.setAttribute("aria-invalid", "true");
+    elError.setAttribute("aria-live", isLive ? "assertive" : "off");
+    elError.classList.add("warning");
+    // el.classList.add("field-outline");
+    label.classList.add("error");
+  }
 }
 
 function updateSubmitSummary({ isSubmit } = {}) {
-    const elSummary = elForm.querySelector(".form-feedback");
-    const elSummaryMsg = elSummary.querySelector(".form-feedback-msg");
+  const elSummary = elForm.querySelector(".form-feedback");
+  const elSummaryMsg = elSummary.querySelector(".form-feedback-msg");
 
-    // Clear form feedback
-    elSummaryMsg.classList.remove("isInvalid");
-    elSummaryMsg.classList.remove("isSuccess");
-    elSummaryMsg.innerText = "";
-    const errorsState = Object.values(formErrors);
+  // Clear form feedback
+  elSummaryMsg.classList.remove("isInvalid");
+  elSummaryMsg.classList.remove("isSuccess");
+  elSummaryMsg.innerText = "";
+  const errorsState = Object.values(formErrors);
 
-    if (errorsState.includes(true)) {
-        elSubmit.classList.add("field-outline");
-        // Show error msg
-        const errorCount = Object.values(formErrors).reduce((total, hasError) => {
-            return hasError ? total + 1 : total;
-        }, 0);
-        const errorMsg =
-            errorCount === 1
-                ? `Failed to submit because ${errorCount} field is invalid.`
-                : `Failed to submit because ${errorCount} fields are invalid.`;
-        elSummaryMsg.classList.add("isInvalid");
-        elSummaryMsg.innerText = errorMsg;
+  if (errorsState.includes(true)) {
+    elSubmit.classList.add("field-outline");
+    // Show error msg
+    const errorCount = Object.values(formErrors).reduce((total, hasError) => {
+      return hasError ? total + 1 : total;
+    }, 0);
+    const errorMsg =
+      errorCount === 1
+        ? `Failed to submit because ${errorCount} field is invalid.`
+        : `Failed to submit because ${errorCount} fields are invalid.`;
+    elSummaryMsg.classList.add("isInvalid");
+    elSummaryMsg.innerText = errorMsg;
 
-        elSummary.querySelector(".form-feedback-sr").innerText = isSubmit
-            ? // Set SR error message only on submit to avoid being re-announced
-            // every time the error summary visually changes.
-            errorMsg
-            : "";
-    } else if (isSubmit) {
-        const successMsg = "Submission successful";
-        elSummary.querySelector(".form-feedback-sr").innerText = successMsg;
-        elSummaryMsg.innerText = successMsg;
-        elSummaryMsg.classList.add("isSuccess");
-
-    }
+    elSummary.querySelector(".form-feedback-sr").innerText = isSubmit
+      ? // Set SR error message only on submit to avoid being re-announced
+        // every time the error summary visually changes.
+        errorMsg
+      : "";
+  } else if (isSubmit) {
+    const successMsg = "Submission successful";
+    elSummary.querySelector(".form-feedback-sr").innerText = successMsg;
+    elSummaryMsg.innerText = successMsg;
+    elSummaryMsg.classList.add("isSuccess");
+  }
 }
 
 elForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    hasSubmitted = true;
+  e.preventDefault();
+  hasSubmitted = true;
 
-    // Validate again
-    validateFieldEmail(elEmail);
-    validateFieldPhone(elPhone);
-    validateFieldName(elName);
-    validateFieldMessage(elMessage);
+  // Validate again
+  validateFieldEmail(elEmail);
+  validateFieldPhone(elPhone);
+  validateFieldName(elName);
+  validateFieldMessage(elMessage);
 
-    updateSubmitSummary({ isSubmit: true });
+  updateSubmitSummary({ isSubmit: true });
 });
