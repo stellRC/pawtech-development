40c1d953aa9b98a56827d22b498cb5815cdb979a
StellRC <38394781+stellRC@users.noreply.github.com>
2025-04-07 08:32:17 +0000

    desktop view

--- script.js
+++ script.js
@@ -116,23 +116,23 @@ window.onload = function () {
   // hiddenSpan.style.display = 'none'
 };
 
-// // MAILTO LINK
-// const links = document.querySelectorAll(
-// 	"[data-part1][data-part2][data-part3][data-subject]"
-// );
-// for (const link of links) {
-// 	const attrs = link.dataset;
-// 	link.setAttribute(
-// 		"href",
-// 		`mailto:${attrs.part1}&#64;${attrs.part2}&#46;${attrs.part3}?subject=${attrs.subject}`
-// 	);
-// 	if (link.classList.contains("contact-add")) {
-// 		link.innerHTML =
-// 			`${atob(attrs.part1)}@` +
-// 			link.innerHTML +
-// 			`${atob(attrs.part2)}.${atob(attrs.part3)}`;
-// 	}
-// }
+// MAILTO LINK
+const links = document.querySelectorAll(
+  "[data-part1][data-part2][data-part3][data-subject]"
+);
+for (const link of links) {
+  const attrs = link.dataset;
+  link.setAttribute(
+    "href",
+    `mailto:${attrs.part1}&#64;${attrs.part2}&#46;${attrs.part3}?subject=${attrs.subject}`
+  );
+  if (link.classList.contains("contact-add")) {
+    link.innerHTML =
+      `${atob(attrs.part1)}@` +
+      link.innerHTML +
+      `${atob(attrs.part2)}.${atob(attrs.part3)}`;
+  }
+}
 
 document.addEventListener("scroll", () => {
   let stickyHeader = document.querySelector(".sticky-header");
@@ -146,15 +146,15 @@ document.addEventListener("scroll", () => {
 });
 
 function showText(e) {
-  if (!e.classList.contains("show")) {
-    e.classList.add("show");
+  if (!e.classList.contains("show-flex")) {
+    e.classList.add("show-flex");
   } else {
-    e.classList.remove("show");
+    e.classList.remove("show-flex");
   }
 }
 
 function hideText(e) {
-  e.classList.remove("show");
+  e.classList.remove("show-flex");
 }
 
 function showMore(e) {
