4eb424ff37884d052f7f6dc84396c4cc901629b5
StellRC <stellaffarce@gmail.com>
2025-06-27 19:37:14 -0700

    hide and hidden

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ for (const link of links) {
 document.addEventListener("scroll", () => {
   let stickyHeader = document.querySelector(".sticky-header");
   for (const child of stickyHeader.children) {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 900) {
       child.classList.remove("hide");
     } else {
       child.classList.add("hide");
