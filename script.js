a15d155837bf0f7b3e17ae4270975298cc114db2
StellRC <stellaffarce@gmail.com>
2025-06-29 11:02:56 -0700

    "

--- script.js
+++ script.js
@@ -174,9 +174,3 @@ function showMore(e) {
     servicesMore.classList.add("show");
   }
 }
-// We listen to the resize event
-// window.addEventListener("resize", () => {
-//   // We execute the same script as before
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty("--vh", `${vh}px`);
-// });
