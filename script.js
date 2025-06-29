c3e90bdf145f152a7131ba2162dbf74c5aaed80e
StellRC <stellaffarce@gmail.com>
2025-06-29 15:50:46 -0700

    "

--- script.js
+++ script.js
@@ -168,11 +168,11 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 // We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+// window.addEventListener("resize", () => {
+//   // We execute the same script as before
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
+// });
 
 document.addEventListener("scroll", function () {
   const stickyHeader = document.querySelector(".sticky-header");
