ea78959f21348f473b6b131667c20078a717d096
StellRC <stellaffarce@gmail.com>
2025-06-29 21:42:28 -0700

    "

--- script.js
+++ script.js
@@ -136,6 +136,9 @@ function modal(e) {
     // e.previousElementSibling.classList.add("link-background");
     e.previousElementSibling.classList.add("clicked");
     bodyElement.classList.add("modal-open");
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
     e.showModal();
   } else {
     hideText(e);
