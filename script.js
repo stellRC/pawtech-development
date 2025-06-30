558307cca71b2ededbe28aa7642b9dce35d539c5
StellRC <stellaffarce@gmail.com>
2025-06-29 21:54:55 -0700

    "

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ document.addEventListener("scroll", function () {
   const contentTop = content.offsetTop;
 
   for (const child of stickyHeader.children) {
-    if (window.scrollY > 750) {
+    if (window.scrollY >= contentTop) {
       child.classList.add("opacity-change");
       stickyHeader.classList.add("fixed");
     } else {
